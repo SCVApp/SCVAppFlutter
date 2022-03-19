d2540d0605cff5a06000866201479c3c7e83eb83
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2022-03-20 00:03:33 +0100

    malce dodelane nastavitve

--- lib/nastavitve.dart
+++ lib/nastavitve.dart
@@ -5,9 +5,10 @@ import 'package:scv_app/prijava.dart';
 import 'package:scv_app/uvod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'data.dart';
+import 'package:babstrap_settings_screen/babstrap_settings_screen.dart';
 
-class NastavitvePage extends StatefulWidget{
-  NastavitvePage({Key key, this.title,this.data}) : super(key: key);
+class NastavitvePage extends StatefulWidget {
+  NastavitvePage({Key key, this.title, this.data}) : super(key: key);
 
   final String title;
 
@@ -16,8 +17,7 @@ class NastavitvePage extends StatefulWidget{
   _NastavitvePageState createState() => _NastavitvePageState();
 }
 
-class _NastavitvePageState extends State<NastavitvePage>{
-
+class _NastavitvePageState extends State<NastavitvePage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   int selectedPickerItem = 0;
@@ -29,69 +29,121 @@ class _NastavitvePageState extends State<NastavitvePage>{
     super.initState();
     loadToken();
   }
-  
-  void loadToken() async{
+
+  void loadToken() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    try{
+    try {
       setState(() {
-         token = prefs.getString(keyForAccessToken);
+        token = prefs.getString(keyForAccessToken);
       });
-    }catch (e){
+    } catch (e) {
       print(e);
     }
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    Future<void> odjava() async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.remove(keyForAccessToken);
+      prefs.remove(keyForRefreshToken);
+      prefs.remove(keyForExpiresOn);
 
-      Future<void> odjava() async {
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        prefs.remove(keyForAccessToken);
-        prefs.remove(keyForRefreshToken);
-        prefs.remove(keyForExpiresOn);
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => OnBoardingPage()));
+    }
 
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => OnBoardingPage()));
-      }
-
-      return Scaffold(
-        key:_scaffoldKey,
-        body: Center(
+    return Scaffold(
+        key: _scaffoldKey,
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.all(24),
+            children: [
+              SimpleUserCard(
+                userName: "Uporabnik",
+                userProfilePic: NetworkImage(
+                    "$apiUrl/user/get/profilePicture?=${widget.data.user.mail}",
+                    headers: {"Authorization": token}),
+              ),
+              SettingsGroup(
+                items: [
+                  SettingsItem(
+                    onTap: () {},
+                    icons: Icons.fingerprint,
+                    iconStyle: IconStyle(
+                      iconsColor: Colors.white,
+                      withBackground: true,
+                      backgroundColor: Colors.red,
+                    ),
+                    title: 'Status',
+                    subtitle: "Spremeni svoj status!",
+                  ),
+                  SettingsItem(
+                    onTap: () {},
+                    icons: Icons.dark_mode_rounded,
+                    iconStyle: IconStyle(
+                      iconsColor: Colors.white,
+                      withBackground: true,
+                      backgroundColor: Colors.red,
+                    ),
+                    title: 'Temni način',
+                    subtitle: "Avtomatsko",
+                    trailing: Switch.adaptive(
+                      value: false,
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+                /* body: Center(
           child: Column(
             children: <Widget>[
               userInfo(odjava),
             ],
-          ),
-        ),
-      );
-  }
-
-  Widget userInfo(odjava){
-
-    return new Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-              userImage(),
-              new Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(widget.data.user.displayName,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),),
-                  Text(widget.data.user.mail)
-                ],
+          ), */
               ),
-              TextButton(onPressed: odjava, child: Image(image: AssetImage("assets/odjava.png"),height: 32,)),
-      ]
-      );
+            ],
+          ),
+        ));
   }
 
-  Widget userImage(){
-    return token == "" ? Image(image: AssetImage("assets/profilePicture.png"),height: 100,) :
-              ClipRRect(
-                borderRadius: BorderRadius.circular(50),
-                child: FadeInImage(
-                  image: NetworkImage("$apiUrl/user/get/profilePicture?=${widget.data.user.mail}",headers: {"Authorization":token})
-                  ,placeholder: AssetImage("assets/profilePicture.png")
-                  ,height: 100,),
-              );
+  Widget userInfo(odjava) {
+    return new Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      userImage(),
+      new Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.data.user.displayName,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          Text(widget.data.user.mail)
+        ],
+      ),
+      TextButton(
+          onPressed: odjava,
+          child: Image(
+            image: AssetImage("assets/odjava.png"),
+            height: 32,
+          )),
+    ]);
+  }
+
+  Widget userImage() {
+    return token == ""
+        ? Image(
+            image: AssetImage("assets/profilePicture.png"),
+            height: 100,
+          )
+        : ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: FadeInImage(
+              image: NetworkImage(
+                  "$apiUrl/user/get/profilePicture?=${widget.data.user.mail}",
+                  headers: {"Authorization": token}),
+              placeholder: AssetImage("assets/profilePicture.png"),
+              height: 100,
+            ),
+          );
   }
 }
