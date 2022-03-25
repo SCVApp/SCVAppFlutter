3f362b355dcd1756c173cd38857c9a09dce90f38
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2022-03-25 23:08:09 +0100

    biometric poskus 1

--- lib/SettingsPages/biometricPage.dart
+++ lib/SettingsPages/biometricPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_picker/flutter_picker.dart';
 import 'package:scv_app/prijava.dart';
@@ -8,7 +9,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../data.dart';
 import 'package:babstrap_settings_screen/babstrap_settings_screen.dart';
 import 'package:get/get.dart';
-
+import 'package:local_auth/local_auth.dart';
 
 class BiometricPage extends StatefulWidget {
   BiometricPage({Key key, this.data}) : super(key: key);
@@ -20,6 +21,34 @@ class BiometricPage extends StatefulWidget {
 
 class _BiometricPage extends State<BiometricPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+  bool _canCheckBiometric = false;
+  String _authorizedOrNot = "Not Authorized";
+  List<BiometricType> _availableBiometricTypes = List<BiometricType>();
+
+  Future<void> _authorizeNow() async {
+    bool isAuthorized = false;
+    try {
+      isAuthorized = await _localAuthentication.authenticate(
+        localizedReason: "Please authenticate to complete your transaction",
+        useErrorDialogs: true,
+        stickyAuth: true,
+      );
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      if (isAuthorized) {
+        _authorizedOrNot = "Authorized";
+      }else{
+        _authorizedOrNot = "Not Authorized";
+      }
+    });
+  }
 
   int selectedPickerItem = 0;
 
@@ -33,20 +62,22 @@ class _BiometricPage extends State<BiometricPage> {
   bool _value = true;
   @override
   Widget build(BuildContext context) {
-  
-
     return Scaffold(
         body: SafeArea(
-          child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(onPressed: (() => Navigator.pop(context)),child: Icon(Icons.arrow_back_ios),),
-              ElevatedButton(child: Icon(Icons.abc),),
-            ],
+            child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: (() => Navigator.pop(context)),
+            child: Icon(Icons.arrow_back_ios),
           ),
-        )
-        )
-    );
+          ElevatedButton(
+            child: Icon(Icons.abc),
+            onPressed: _authorizeNow,
+          ),
+        ],
+      ),
+    )));
   }
 }
