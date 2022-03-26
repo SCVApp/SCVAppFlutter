6d9992ab9e4e1eeff661810e24a16e547f2d3631
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2022-03-26 10:44:47 +0100

    Update biometricPage.dart

--- lib/SettingsPages/biometricPage.dart
+++ lib/SettingsPages/biometricPage.dart
@@ -63,59 +63,6 @@ class _BiometricPage extends State<BiometricPage> {
   }
 
   bool _value = true;
-  @override
-  Widget buildAvailability(BuildContext context) => buildButton(
-        text: 'Check Availability',
-        icon: Icons.event_available,
-        onClicked: () async {
-          final isAvailable = await LocalAuthApi.hasBiometrics();
-          final biometrics = await LocalAuthApi.getBiometrics();
-
-          final hasFingerprint = biometrics.contains(BiometricType.fingerprint);
-
-          showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title: Text('Availability'),
-              content: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  buildText('Biometrics', isAvailable),
-                  buildText('Fingerprint', hasFingerprint),
-                ],
-              ),
-            ),
-          );
-        },
-      );
-
-  Widget buildText(String text, bool checked) => Container(
-        margin: EdgeInsets.symmetric(vertical: 8),
-        child: Row(
-          children: [
-            checked
-                ? Icon(Icons.check, color: Colors.green, size: 24)
-                : Icon(Icons.close, color: Colors.red, size: 24),
-            const SizedBox(width: 12),
-            Text(text, style: TextStyle(fontSize: 24)),
-          ],
-        ),
-      );
-
-  Widget buildAuthenticate(BuildContext context) => buildButton(
-        text: 'Authenticate',
-        icon: Icons.lock_open,
-        onClicked: () async {
-          final isAuthenticated = await LocalAuthApi.authenticate();
-
-          if (isAuthenticated) {
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => UrnikPage()),
-            );
-          }
-        },
-      );
 
   Widget buildButton({
     @required String text,
