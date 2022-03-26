136cee64d8549f5f79388be1d2067bf825a91ee1
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2022-03-26 10:19:36 +0100

    testing biometrics

--- android/app/src/main/kotlin/com/example/scv_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/scv_app/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.scv_app
 
-import io.flutter.embedding.android.FlutterActivity
+iimport io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.engine.FlutterEngine
+import androidx.annotation.NonNull;
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
--- lib/SettingsPages/biometricPage.dart
+++ lib/SettingsPages/biometricPage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_picker/flutter_picker.dart';
+import 'package:scv_app/nastavitve.dart';
 import 'package:scv_app/prijava.dart';
 import 'package:scv_app/uvod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,6 +11,8 @@ import '../data.dart';
 import 'package:babstrap_settings_screen/babstrap_settings_screen.dart';
 import 'package:get/get.dart';
 import 'package:local_auth/local_auth.dart';
+import 'package:scv_app/api/local_auth_api.dart';
+
 
 class BiometricPage extends StatefulWidget {
   BiometricPage({Key key, this.data}) : super(key: key);
@@ -61,6 +64,77 @@ class _BiometricPage extends State<BiometricPage> {
 
   bool _value = true;
   @override
+
+
+  Widget buildAvailability(BuildContext context) => buildButton(
+        text: 'Check Availability',
+        icon: Icons.event_available,
+        onClicked: () async {
+          final isAvailable = await LocalAuthApi.hasBiometrics();
+          final biometrics = await LocalAuthApi.getBiometrics();
+
+          final hasFingerprint = biometrics.contains(BiometricType.fingerprint);
+
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Availability'),
+              content: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  buildText('Biometrics', isAvailable),
+                  buildText('Fingerprint', hasFingerprint),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+
+  Widget buildText(String text, bool checked) => Container(
+        margin: EdgeInsets.symmetric(vertical: 8),
+        child: Row(
+          children: [
+            checked
+                ? Icon(Icons.check, color: Colors.green, size: 24)
+                : Icon(Icons.close, color: Colors.red, size: 24),
+            const SizedBox(width: 12),
+            Text(text, style: TextStyle(fontSize: 24)),
+          ],
+        ),
+      );
+
+  Widget buildAuthenticate(BuildContext context) => buildButton(
+        text: 'Authenticate',
+        icon: Icons.lock_open,
+        onClicked: () async {
+          final isAuthenticated = await LocalAuthApi.authenticate();
+
+          if (isAuthenticated) {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => NastavitvePage()),
+            );
+          }
+        },
+      );
+
+  Widget buildButton({
+    @required String text,
+    @required IconData icon,
+    @required VoidCallback onClicked,
+  }) =>
+      ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size.fromHeight(50),
+        ),
+        icon: Icon(icon, size: 26),
+        label: Text(
+          text,
+          style: TextStyle(fontSize: 20),
+        ),
+        onPressed: onClicked,
+      );
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
@@ -74,10 +148,12 @@ class _BiometricPage extends State<BiometricPage> {
           ),
           ElevatedButton(
             child: Icon(Icons.abc),
-            onPressed: _authorizeNow,
+            onPressed: _authorizeNow
           ),
         ],
       ),
     )));
+  
   }
+  
 }
