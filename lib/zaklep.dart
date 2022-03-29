393acdaa10ca247fd51d362d04668fd35faf792b
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2022-03-29 13:15:35 +0200

    Update zaklep.dart

--- lib/zaklep.dart
+++ lib/zaklep.dart
@@ -95,7 +95,7 @@ class _ZaklepPageState extends State<ZaklepPage> {
             _authorizeNow();
           })
           ],
-          ):Text(""),
+          ): Center(child:Text("")),
     ));
   }
 }
