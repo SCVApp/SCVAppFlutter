ca0654a0e08105d127ab49f39cd1e5579aaeef52
Urban Krepel <urban.kreppel@gmail.com>
2022-04-04 08:55:55 +0200

    Skritje tipkovnice ko gres iz pisanja malice

--- lib/malice.dart
+++ lib/malice.dart
@@ -28,74 +28,76 @@ class _MalicePageState extends State<MalicePage> {
     //     _myController = c;
     //   });
 
-    body:
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Text(
-            "Prijava v sistem malic",
-            style: TextStyle(fontSize: 25),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 60.0),
-            child: Center(
-              child: Container(
-                  width: 200,
-                  height: 150,
-                  child: Image.asset('assets/school_logo.png')),
+    return GestureDetector(
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Prijava v sistem malic",
+              style: TextStyle(fontSize: 25),
             ),
-          ),
-          Padding(
-            //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: TextField(
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    width: 200,
+                    height: 150,
+                    child: Image.asset('assets/school_logo.png')),
+              ),
+            ),
+            Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintStyle: TextStyle(color: Colors.white),
+                      labelStyle: TextStyle(color: Colors.white),
+                      labelText: 'E-pošta',
+                      hintText: 'v formatu ime.priimek@scv.si')),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 10, bottom: 20),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
                     hintStyle: TextStyle(color: Colors.white),
                     labelStyle: TextStyle(color: Colors.white),
-                    labelText: 'E-pošta',
-                    hintText: 'v formatu ime.priimek@scv.si')),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-                left: 15.0, right: 15.0, top: 10, bottom: 20),
-            //padding: EdgeInsets.symmetric(horizontal: 15),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  hintStyle: TextStyle(color: Colors.white),
-                  labelStyle: TextStyle(color: Colors.white),
-                  border: OutlineInputBorder(),
-                  labelText: 'Geslo',
-                  hintText: 'Geslo, uporabljeno za malice'),
-            ),
-          ),
-          Container(
-            height: 50,
-            width: 250,
-            decoration: BoxDecoration(
-                color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-            child: TextButton(
-              onPressed: ()=>Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MainMalicePage())),
-              child: Text(
-                'Prijava',
-                style: TextStyle(color: Colors.white, fontSize: 25),
+                    border: OutlineInputBorder(),
+                    labelText: 'Geslo',
+                    hintText: 'Geslo, uporabljeno za malice'),
               ),
             ),
-          ),
-          Container(
-            height: 100,
-            width: 250,
-            child: TextButton(
-              child: Text(
-                'Pozabljeno geslo?',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: ()=>Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MainMalicePage())),
+                child: Text(
+                  'Prijava',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
               ),
-              onPressed: ()=>launch("https://malice.scv.si/students/password/new"),
             ),
-          ),
-        ],
+            Container(
+              height: 100,
+              width: 250,
+              child: TextButton(
+                child: Text(
+                  'Pozabljeno geslo?',
+                  style: TextStyle(color: Colors.blue, fontSize: 15),
+                ),
+                onPressed: ()=>launch("https://malice.scv.si/students/password/new"),
+              ),
+            ),
+          ],
+        ),
       ),
+      onTap: ()=> FocusManager.instance.primaryFocus.unfocus()
     );
 
     /* return Scaffold(
