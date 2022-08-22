ab0576179e6bdd894c18d58a8e40eb8ac1126c23
Urban Krepel <urban.kreppel@gmail.com>
2022-08-22 12:36:56 +0200

    Uredil zaklepni zaslon

--- lib/zaklep.dart
+++ lib/zaklep.dart
@@ -1,6 +1,7 @@
 import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:scv_app/data.dart';
 import 'package:scv_app/functions.dart';
 import 'package:scv_app/main.dart';
 import 'package:local_auth/local_auth.dart';
@@ -82,7 +83,7 @@ class _ZaklepPageState extends State<ZaklepPage> {
     bool isAuthorized = false;
     try {
       isAuthorized = await _localAuthentication.authenticate(
-          localizedReason: "Za vstop se autoriziraj:",
+          localizedReason: "Za vstop se autoriziraj",
           options:
               AuthenticationOptions(useErrorDialogs: true, stickyAuth: true));
     } on PlatformException catch (e) {
@@ -125,6 +126,8 @@ class _ZaklepPageState extends State<ZaklepPage> {
     await _checkBiometric();
     if (_canCheckBiometric) {
       await _authorizeNow();
+    } else {
+      _OpozoriloBiometrika();
     }
   }
 
@@ -134,17 +137,55 @@ class _ZaklepPageState extends State<ZaklepPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: !_authorizedOrNot
-          ? Column(
-              children: [
-                ElevatedButton(
-                    child: Icon(Icons.error_outline),
-                    onPressed: () {
-                      _authorizeNow();
-                    })
-              ],
-            )
-          : Text(""),
-    ));
+            child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(9), //add border radius
+            child: Image.asset(
+              "assets/1024.png",
+              width: MediaQuery.of(context).size.width * 0.3,
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(bottom: 25)),
+          Text(
+            "Prijava v sistem ŠCVApp",
+          ),
+          Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).size.height * 0.27)),
+          !_authorizedOrNot ? button() : CircularProgressIndicator(),
+        ],
+      ),
+    )));
+  }
+
+  Widget button() {
+    double widget = MediaQuery.of(context).size.width * 0.3;
+    double height = widget * 0.36;
+    double textSize = height * 0.37;
+    return LayoutBuilder(builder: ((context, constraints) {
+      return TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+              color: HexColor.fromHex("#16A7D4"),
+              borderRadius: BorderRadius.circular(7),
+            ),
+            width: widget,
+            height: height,
+            child: Center(
+                child: Text(
+              "Avtorizacija",
+              style: TextStyle(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  fontSize: textSize),
+            )),
+          ),
+          onPressed: () {
+            onStartUp();
+          });
+    }));
   }
 }
