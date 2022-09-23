3aeffec8edb4a56bcefc9efecba20f5f79e3d875
Urban Krepel <urban.kreppel@gmail.com>
2022-09-23 22:35:58 +0200

    Added spacing between elements

--- lib/Data/functions.dart
+++ lib/Data/functions.dart
@@ -24,3 +24,12 @@ Future<void> logOutUser(BuildContext context) async {
   Navigator.of(context).pushReplacement(
       MaterialPageRoute(builder: (context) => OnBoardingPage()));
 }
+
+extension ListSpaceBetweenExtension on List<Widget> {
+  List<Widget> withSpaceBetween({double spacing}) => [
+        for (int i = 0; i < this.length; i++) ...[
+          if (i > 0) Padding(padding: EdgeInsets.only(bottom: spacing)),
+          this[i],
+        ],
+      ];
+}
--- lib/DoorUnlock/DoorUnlockUser.dart
+++ lib/DoorUnlock/DoorUnlockUser.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 import 'package:scv_app/Components/backBtn.dart';
-import 'package:scv_app/DoorUnlock/components/DoorCardWidget.dart';
-import 'package:scv_app/DoorUnlock/components/DoorCardsWidget.dart';
+import 'package:scv_app/Data/functions.dart';
 import '../Data/data.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -24,42 +22,45 @@ class _DoorUnlockUserPage extends State<DoorUnlockUserPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-            child: Wrap(
-      spacing: 20,
-      direction: Axis.vertical,
-      alignment: WrapAlignment.center,
+            child: Column(
       children: [
         backButton(context),
-        Container(
-          height: (MediaQuery.of(context).size.width * 0.5) + 40,
-          //add svg asset image
-          child: SvgPicture.asset(
-            'assets/Door_Closed.svg',
-            color: Colors.green,
-            height: (MediaQuery.of(context).size.width * 0.5) + 20,
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("Izbrana učilnica:"),
-            Text("C503"),
-          ],
-        ),
-        //button with lock icon and text Odlkleni vrata
-        FloatingActionButton.extended(
-          onPressed: () {},
-          icon: Icon(
-            Icons.lock,
-            size: 23,
-          ),
-          label: const Text("Odkleni vrata"),
-          backgroundColor: Colors.green,
-          foregroundColor: Colors.white,
-        ),
-        Container(
-          child: Text("Vrata uspešno odklenjena!"),
-        ),
+        Padding(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: (MediaQuery.of(context).size.width * 0.5) + 40,
+                  //add svg asset image
+                  child: SvgPicture.asset(
+                    'assets/Door_Closed.svg',
+                    color: Colors.green,
+                    height: (MediaQuery.of(context).size.width * 0.5) + 20,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Izbrana učilnica:"),
+                    Text("C503"),
+                  ],
+                ),
+                FloatingActionButton.extended(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.lock,
+                    size: 23,
+                  ),
+                  label: const Text("Odkleni vrata"),
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                ),
+                Container(
+                  child: Text("Vrata uspešno odklenjena!"),
+                ),
+              ].withSpaceBetween(spacing: 30)),
+          padding: EdgeInsets.symmetric(horizontal: 40),
+        )
       ],
     )));
   }
