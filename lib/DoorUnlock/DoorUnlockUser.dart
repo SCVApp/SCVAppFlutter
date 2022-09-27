2438b73a4e014a4c417d74da2d0a0aa931abdaf7
Urban Krepel <urban.kreppel@gmail.com>
2022-09-27 11:46:21 +0200

    Naredil nov design za odklepanje vrat

--- lib/DoorUnlock/DoorUnlockUser.dart
+++ lib/DoorUnlock/DoorUnlockUser.dart
@@ -1,3 +1,4 @@
+import 'package:animate_icons/animate_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:scv_app/Components/backBtn.dart';
@@ -6,9 +7,10 @@ import '../Data/data.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class DoorUnlockUserPage extends StatefulWidget {
-  DoorUnlockUserPage({Key key, this.data}) : super(key: key);
+  DoorUnlockUserPage({Key key, this.data, this.doorCode}) : super(key: key);
 
   final Data data;
+  final String doorCode;
 
   _DoorUnlockUserPage createState() => _DoorUnlockUserPage();
 }
@@ -19,6 +21,18 @@ class _DoorUnlockUserPage extends State<DoorUnlockUserPage> {
     super.initState();
   }
 
+  ThemeColorForStatus themeColorForStatus = ThemeColorForStatus.unknown;
+  bool isLoading = false;
+
+  unlockDoor() async {
+    setState(() {
+      isLoading = true;
+    });
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,62 +40,86 @@ class _DoorUnlockUserPage extends State<DoorUnlockUserPage> {
             child: Column(
       children: [
         backButton(context),
-        Padding(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  height: (MediaQuery.of(context).size.width * 0.5) + 40,
-                  //add svg asset image
-                  child: SvgPicture.asset(
-                    'assets/Door_Closed.svg',
-                    color: Colors.green,
-                    height: (MediaQuery.of(context).size.width * 0.5) + 20,
+        Padding(padding: EdgeInsets.only(top: 10)),
+        Expanded(
+            child: ListView(
+                children: [
+          Padding(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Izbrana učilnica:",
+                    style: TextStyle(fontSize: 18),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Izbrana učilnica:"),
-                    Text("C503"),
-                  ],
-                ),
-                FloatingActionButton.extended(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.lock,
-                    size: 23,
+                  Text(
+                    "C503",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  label: const Text("Odkleni vrata"),
-                  backgroundColor: Colors.green,
-                  foregroundColor: Colors.white,
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    print("You pressed Icon Elevated Button");
-                  },
-                  icon: Icon(Icons.lock), //icon data for elevated button
-                  label: Text("Odkleni vrata"), //label text
-                  style: ButtonStyle(
-                      backgroundColor: getColor(Colors.red, Colors.green)),
-                ),
-                Container(
-                  child: Text("Vrata uspešno odklenjena!"),
-                ),
-              ].withSpaceBetween(spacing: 30)),
-          padding: EdgeInsets.symmetric(horizontal: 100),
-        )
+                ],
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 70)),
+          GestureDetector(
+              child: Container(
+                  width: MediaQuery.of(context).size.width * 0.65,
+                  height: MediaQuery.of(context).size.width * 0.65,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.transparent,
+                      border: Border.all(
+                          color: this.themeColorForStatus.color, width: 20)),
+                  child: Center(
+                      child: !isLoading
+                          ? Icon(
+                              Icons.lock_outline,
+                              color: this.themeColorForStatus.color,
+                              size: MediaQuery.of(context).size.width * 0.3,
+                            )
+                          : CircularProgressIndicator(
+                              color: this.themeColorForStatus.color,
+                            ))),
+              onTap: unlockDoor),
+          !isLoading
+              ? Text(
+                  "Odklep je na voljo",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                )
+              : SizedBox(),
+          !isLoading
+              ? Text("Pritisni ključavnico za odklep",
+                  style: TextStyle(fontSize: 15), textAlign: TextAlign.center)
+              : SizedBox(),
+        ].withSpaceBetween(spacing: 30))),
       ],
     )));
   }
+}
 
-//change color of a button on press
-  getColor(Color color1, Color color2) {
-    return MaterialStateProperty.resolveWith<Color>((states) {
-      if (states.contains(MaterialState.pressed)) {
-        return color1;
-      }
-      return color2;
-    });
+enum ThemeColorForStatus {
+  success,
+  promisson_denied,
+  error,
+  unknown,
+}
+
+extension ThemeColorForStatusExtension on ThemeColorForStatus {
+  Color get color {
+    switch (this) {
+      case ThemeColorForStatus.success:
+        return Colors.green;
+      case ThemeColorForStatus.promisson_denied:
+        return Colors.orange;
+      case ThemeColorForStatus.error:
+        return Colors.red;
+      case ThemeColorForStatus.unknown:
+        return Colors.grey;
+      default:
+        return Colors.black;
+    }
   }
 }
