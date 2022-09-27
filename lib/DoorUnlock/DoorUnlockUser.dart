1ddb91b41033964d3a4bb0482457b81de474501b
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2022-09-27 09:02:47 +0200

    Dodelan gumb

--- lib/DoorUnlock/DoorUnlockUser.dart
+++ lib/DoorUnlock/DoorUnlockUser.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:scv_app/Components/backBtn.dart';
 import 'package:scv_app/Data/functions.dart';
 import '../Data/data.dart';
@@ -55,13 +56,32 @@ class _DoorUnlockUserPage extends State<DoorUnlockUserPage> {
                   backgroundColor: Colors.green,
                   foregroundColor: Colors.white,
                 ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    print("You pressed Icon Elevated Button");
+                  },
+                  icon: Icon(Icons.lock), //icon data for elevated button
+                  label: Text("Odkleni vrata"), //label text
+                  style: ButtonStyle(
+                      backgroundColor: getColor(Colors.red, Colors.green)),
+                ),
                 Container(
                   child: Text("Vrata uspešno odklenjena!"),
                 ),
               ].withSpaceBetween(spacing: 30)),
-          padding: EdgeInsets.symmetric(horizontal: 40),
+          padding: EdgeInsets.symmetric(horizontal: 100),
         )
       ],
     )));
+  }
+
+//change color of a button on press
+  getColor(Color color1, Color color2) {
+    return MaterialStateProperty.resolveWith<Color>((states) {
+      if (states.contains(MaterialState.pressed)) {
+        return color1;
+      }
+      return color2;
+    });
   }
 }
