1f17ad2967a18e580557d38832b56e5049131f9c
Urban Krepel <urban.kreppel@gmail.com>
2024-03-05 17:21:46 +0100

    update

--- lib/api/malice/malica.dart
+++ lib/api/malice/malica.dart
@@ -4,7 +4,7 @@ class Malica {
   static final String apiURL = "https://malice.scv.si/api/v2";
   final MalicaUser maliceUser = new MalicaUser();
 
-  String? _afterLoginURL = "https://malice.scv.si/?date=8-3-2024";
+  String? _afterLoginURL;
   get afterLoginURL => _afterLoginURL;
 
   setAfterLoginURL(String url) {
