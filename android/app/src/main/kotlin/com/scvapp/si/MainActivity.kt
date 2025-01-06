03959f32ad53b489764e493727ef6d61bf6503e2
Urban Krepel <urban.kreppel@gmail.com>
2025-01-06 16:04:34 +0100

    fixed android stuff

--- android/app/src/main/kotlin/com/scvapp/si/MainActivity.kt
+++ android/app/src/main/kotlin/com/scvapp/si/MainActivity.kt
@@ -1,9 +1,9 @@
-package com.example.scv_app
+package com.scvapp.si
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.embedding.engine.FlutterEngine
-import androidx.annotation.NonNull;
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterFragmentActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
