e53bb371a3d2117d93d32f87e2df875b9b6bf5d2
Urban Krepel <urban.kreppel@gmail.com>
2025-02-14 08:03:09 +0100

    new version

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -21,7 +21,7 @@ late BuildContext globalBuildContext;
 // final String apiUrl = "http://localhost:5050";
 final String apiUrl = "https://backend.app.scv.si";
 
-final String appVersion = "2.6.5";
+final String appVersion = "2.6.7";
 
 Connectivity connectivity = new Connectivity();
 
